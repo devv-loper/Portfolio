158ffd04f09cbc612ad434b84cb5d43eb9c37083
Devv Sonii <devpsoni123@gmail.com>
2025-08-19 12:12:53 +0530

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { DeviceProvider } from "@/context/DeviceContext";
-
+import { Analytics } from "@vercel/analytics/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -30,6 +30,7 @@ export default function RootLayout({
       >
         <DeviceProvider>
           {children}
+          <Analytics/>
         </DeviceProvider>
       </body>
     </html>
